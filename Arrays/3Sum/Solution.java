927d8abd5cba6da75b18e712765b75c96f1e1deb
Anuj Sinha <anujsinha789@gmail.com>
2022-01-24 22:27:01 +0530

    Setting up

---
+++ Arrays/3Sum/Solution.java
@@ -0,0 +1,8 @@
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+
+ class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        // Write your code here.
+    }
+}
